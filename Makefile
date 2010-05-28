e7f28d0dee2eda13999265994486df868b2f64d1
Tuomas Starck <dev@starck.fi>
2010-05-28 04:43:09 +0300

    Hello commit!

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Makefile
+
+CC=gcc -ansi -pedantic -Wall
+
+main:
+	$(CC) -o hello hello.c
---
+++ hello.c
@@ -0,0 +1,8 @@
+/* hello.c */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello world!\n");
+	return 0;
+}
